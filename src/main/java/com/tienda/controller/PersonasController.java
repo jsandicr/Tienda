60679f094b21533b9f3414d034c0cb4d77147a87
jsand <you@example.com>
2022-02-21 21:04:19 -0600

    Se modifico el personasController Se agrego el html de agregarPersona y de modificarPersona Se creo un layout llamado plantilla.html

--- src/main/java/com/tienda/controller/PersonasController.java
+++ src/main/java/com/tienda/controller/PersonasController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PersonasController{
@@ -20,5 +22,19 @@ public class PersonasController{
         model.addAttribute("personas",listaPersonas);
         System.out.println("Registros\n"+listaPersonas);
         return "personas";
+    }
+
+    @GetMapping("/modificarPersona/{id}")
+    public String modificarPersona(@PathVariable Long id, Model model){
+        Persona persona=personaService.getPersonById(id);
+        model.addAttribute("persona", persona);
+        System.out.println(persona);
+        return "modificarPersona";
+    }
+
+    @PostMapping("/guardarPersona")
+    public String guardarPersona(Persona persona){
+        personaService.savePerson(persona);
+        return "redirect:/personas";
     }
 }
