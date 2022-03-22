9e3de686bfc266b3ab2ec384981ef5e89a1b96ac
jsand <you@example.com>
2022-03-21 20:37:21 -0600

    Se creo el WebConfig con las configuraciones para idiomas Se incluyo el idioma español e ingles en messages.properties

---
+++ src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,40 @@
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+/*
+Internalizacion ---- i18n --- 18 letras entre la i y la n
+Locale:Representa es el lenguaje, la region geografica, variantes del dialecto de un usuario
+SessionLocaleResolver:Guardar el Locale seleccionado por un usuario como atributo en el request HTTP
+LocalChangeInterceptor:Detectar cualquier cambio de parte del usuario hacia lo que es el Locale
+*/
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Bean
+    public SessionLocaleResolver localeResolver(){
+        var slr=new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("en"));
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci=new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
